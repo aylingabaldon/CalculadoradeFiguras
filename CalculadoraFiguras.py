fecb9918cc42c191318f316b7b056ef3e10f3434
aylingabaldon <aylingabaldon@gmail.com>
2025-10-15 08:58:02 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -4,3 +4,8 @@ st.title("Calculadora de Figuras Geométricas 💠")
 figura = ["Círculo", "Triángulo", "Rectángulo", "Cuadrado"]
 figura_seleccionada = st.selectbox("Selecciona la figura", figura)
 st.write("Figura: ", figura_seleccionada)
+
+if figura == Círculo:
+  operacion= ["Área", "Perímetro"]
+  operacion_seleccionada = st.selectbox("Selecciona la operación", operacion)
+  
