87d030ad840f424e6cb5f0f11afd0e5f762db901
aylingabaldon <aylingabaldon@gmail.com>
2025-10-15 09:08:48 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -5,7 +5,11 @@ figura = ["Círculo", "Triángulo", "Rectángulo", "Cuadrado"]
 figura_seleccionada = st.selectbox("Selecciona la figura", figura)
 st.write("Figura: ", figura_seleccionada)
 
-if figura == Círculo:
+def area_circulo(radio):
+  return math.pi * radio ** 2
+  radio = input("Ingresa el radio:")
+  print(area_circulo(radio))
+if figura == circulo:
   operacion= ["Área", "Perímetro"]
   operacion_seleccionada = st.selectbox("Selecciona la operación", operacion)
   
