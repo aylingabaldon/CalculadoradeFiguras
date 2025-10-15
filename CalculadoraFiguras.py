f82a9ac26f407bb22cdee8c6ceef66cb0ae3fa1f
aylingabaldon <aylingabaldon@gmail.com>
2025-10-15 09:54:52 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -10,7 +10,7 @@ st.write("Figura: ", figura_seleccionada)
 if figura == "Círculo":
  print("Fórmula: ", st.latex("A=π * r²"))
  st.number_input("Radio")
- def area_circulo("Radio"):
+ def area_circulo("radio"):
    return math.pi * radio ** 2
  print(st.success(area_circulo))
 
