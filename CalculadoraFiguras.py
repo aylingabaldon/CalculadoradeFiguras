166c228d32964e5ce21d35ae661d4092811c73d6
aylingabaldon <aylingabaldon@gmail.com>
2025-10-20 03:33:19 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -131,6 +131,7 @@ with tabs[2]:
     funciones = st.selectbox("Selecciona la función:", ["Fórmula General🌳", "Teorema de Pitágoras📐", "Conversión de Temperaturas🌡️"])
     
     if funciones == "Fórmula General🌳":
+        st.header("Fórmula General🌳")
         a = st.number_input("Variable a", min_value=0.0, value=1.0)
         b = st.number_input("Variable b", min_value=0.0, value=1.0)
         c = st.number_input("Variable c", min_value=0.0, value=1.0)
@@ -141,6 +142,7 @@ with tabs[2]:
         st.image("https://dicciomat.com/wp-content/uploads/2024/09/la-formula-general.png")
     
     elif funciones == "Teorema de Pitágoras📐":
+        st.header("Teorema de Pitágoras📐")
         Cateto1 = st.number_input("Cateto 1: ", min_value=0, value=1)
         Cateto2 = st.number_input("Cateto 2: ", min_value=0, value=1)
         hipotenusa = math.sqrt(Cateto1**2 + Cateto2**2)
@@ -148,6 +150,7 @@ with tabs[2]:
         st.image("https://www.smartick.es/blog/wp-content/uploads/2024/02/teorema-de-Pitagoras-2-1024x825.png")
      
     elif funciones == "Conversión de Temperaturas🌡️":
+        st.header("Conversión de Temperaturas🌡️")
           celsius = st.slider("Grados Celsius: ", -100, 100, 0)
           conversion_a_farenheit = ((celsius * 9/5) + 32)
           st.metric("Conversión: ", f"{conversion_a_farenheit:.2f}")
