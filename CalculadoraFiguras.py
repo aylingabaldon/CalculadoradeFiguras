e1915c979f08baf77550324db81df8f8c7eced40
aylingabaldon <aylingabaldon@gmail.com>
2025-10-19 21:05:57 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -6,9 +6,12 @@ st.sidebar.write("Nombre: Aylín Yareli Gabaldón Yáñez")
 st.sidebar.write("Matrícula: 313765")
 tabs = st.tabs("Figuras Geométricas ❀", "Funciones Trigonométricas 〰️")
 
-figura = ["Círculo", "Triángulo", "Rectángulo", "Cuadrado"]
-figura_seleccionada = st.selectbox("Selecciona la figura", figura)
-st.write("Figura: ", figura_seleccionada)
+with tab[0]:
+   st.header("Figuras Geométricas ❀")
+   figura = ["Círculo", "Triángulo", "Rectángulo", "Cuadrado"]
+   figura_seleccionada = st.selectbox("Selecciona la figura", figura)
+   st.write("Figura: ", figura_seleccionada)
+   color = st.color_picker("Selecciona un color: ", "#1f77b4")
 
 if figura == "Círculo":
    print("Fórmula: ", st.latex("A=π * r²"))
