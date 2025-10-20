7df7187665d69f3023055c6dfe44fe44d42f29b2
aylingabaldon <aylingabaldon@gmail.com>
2025-10-19 21:59:37 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -15,9 +15,9 @@ with tabs[0]:
 
 if figura == "Círculo":
    r = st.number_input("Radio", min_value=0.0, value=1.0)
-   st.latex("Fórmula Área: A=C * r²")
+   st.write("Fórmula Área: A=C * r²")
    area = np.pi * r**2
-   st.latex("Fórmula Perímetro: P=2*π*r")         
+   st.write("Fórmula Perímetro: P=2*π*r")         
    perimetro = 2 * np.pi * r
    
 elif figura == "Triángulo":
@@ -25,24 +25,24 @@ elif figura == "Triángulo":
    b = st.number_input("Lado b (Base)", min_value=0.0, value=1.0)
    c = st.number_input("Lado c", min_value=0.0, value=1.0)
    h = st.number_input("Altura: ", min_value=0.0, value=1.0)
-   st.latex("Fórmula Área: (b * h)/2")
+   st.write("Fórmula Área: (b * h)/2")
    area = (b * h) / 2
-   st.latex("Fórmula Perímetro: a + b + c")
+   st.write("Fórmula Perímetro: a + b + c")
    perimetro = a + b + c
 
 elif figura == "Rectángulo":
     b = st.number_input("Base: ", min_value=0.0, value=1.0)
     h = st.number_input("Altura: ", min_value=0.0, value=2.0)
-    st.latex("Fórmula Área: b * h")
+    st.write("Fórmula Área: b * h")
     area = b * h
-    st.latex("Fórmula Perímetro: 2 * (b + h)")
+    st.write("Fórmula Perímetro: 2 * (b + h)")
     perimetro = 2 * (b + h)
 
 elif figura == "Cuadrado":
     l = st.number_input("Lado: ", min_value=0.0, value=1.0)
-    st.latex("Fórmula Área: L * L")
+    st.write("Fórmula Área: L * L")
     area = l**2
-    st.latex("Fórmula Perímetro: 4*L")
+    st.write("Fórmula Perímetro: 4*L")
     perimetro = 4 * l
 
 st.success(f"Área = {area:.2f}")
