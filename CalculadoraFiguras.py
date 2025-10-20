be49c678daf8f6b4519e5902772c90922bd3c4a5
aylingabaldon <aylingabaldon@gmail.com>
2025-10-19 23:39:59 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -32,10 +32,10 @@ with tabs[0]:
         st.latex("Fórmula Perímetro: 2 * π * r")
         st.metric("Perímetro del Círculo: ", f"{perimetro:.2f}")
     fig, ax = plt.subplots()
-        circle = plt.Circle((0, 0), radio, color=color, fill=True)
-        ax.add_patch(circle)
-        ax.set_xlim(radio, radio)
-        ax.set_ylim(radio, radio)
+    circle = plt.Circle((0, 0), radio, color=color, fill=True)
+    ax.add_patch(circle)
+    ax.set_xlim(radio, radio)
+    ax.set_ylim(radio, radio)
 
 
     elif figura == "Triángulo":
