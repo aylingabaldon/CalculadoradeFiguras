9331250eec693f7718265c44688ef7914f558697
aylingabaldon <aylingabaldon@gmail.com>
2025-10-19 21:48:48 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -21,29 +21,29 @@ if figura == "Círculo":
    perimetro = 2 * np.pi * r
    
  elif figura == "Triángulo":
-    a = st.number_input("Lado a", min_value=0.0, value=1.0) 
-    b = st.number_input("Lado b (Base)", min_value=0.0, value=1.0)
-    c = st.number_input("Lado c", min_value=0.0, value=1.0)
-    h = st.number_input("Altura: ", min_value=0.0, value=1.0)
-    st.latex("Fórmula Área: (b * h)/2")
-    area = (b * h) / 2
-    st.latex("Fórmula Perímetro: a + b + c")
-    perimetro = a + b + c
+     a = st.number_input("Lado a", min_value=0.0, value=1.0) 
+     b = st.number_input("Lado b (Base)", min_value=0.0, value=1.0)
+     c = st.number_input("Lado c", min_value=0.0, value=1.0)
+     h = st.number_input("Altura: ", min_value=0.0, value=1.0)
+     st.latex("Fórmula Área: (b * h)/2")
+     area = (b * h) / 2
+     st.latex("Fórmula Perímetro: a + b + c")
+     perimetro = a + b + c
 
 elif figura == "Rectángulo":
-   b = st.number_input("Base: ", min_value=0.0, value=1.0)
-   h = st.number_input("Altura: ", min_value=0.0, value=2.0)
-   st.latex("Fórmula Área: b * h")
-   area = b * h
-   st.latex("Fórmula Perímetro: 2 * (b + h)")
-   perimetro = 2 * (b + h)
+    b = st.number_input("Base: ", min_value=0.0, value=1.0)
+    h = st.number_input("Altura: ", min_value=0.0, value=2.0)
+    st.latex("Fórmula Área: b * h")
+    area = b * h
+    st.latex("Fórmula Perímetro: 2 * (b + h)")
+    perimetro = 2 * (b + h)
 
 elif figura == "Cuadrado":
-   l = st.number_input("Lado: ", min_value=0.0, value=1.0)
-   st.latex("Fórmula Área: L * L")
-   area = l**2
-   st.latex("Fórmula Perímetro: 4*L")
-   perimetro = 4 * l
+    l = st.number_input("Lado: ", min_value=0.0, value=1.0)
+    st.latex("Fórmula Área: L * L")
+    area = l**2
+    st.latex("Fórmula Perímetro: 4*L")
+    perimetro = 4 * l
 
 st.success(f"Área = {area:.2f})
 st.success(f"Perímetro = {perimetro:.2f}) 
