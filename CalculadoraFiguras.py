054f1b82e00bc2fcb168ccf56ebd10b0907d9eaf
aylingabaldon <aylingabaldon@gmail.com>
2025-10-19 23:44:23 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -64,8 +64,9 @@ with tabs[0]:
         st.latex("Fórmula Perímetro: 4 * L")
         st.metric("Perímetro del Cuadrado: ", f"{perimetro:.2f}")
     
-    fig, ax = plt.subplots()
+   
     if figura == "C´riculo":
+        fig, ax = plt.subplots()
         circle = plt.Circle((0, 0), radio, color=color, fill=True)
         ax.add_patch(circle)
         ax.set_xlim(radio, radio)
