3fda53f8a0e8ac10ef634d667cd3ad19fb6ed297
aylingabaldon <aylingabaldon@gmail.com>
2025-10-19 21:12:42 -0600

    Update CalculadoraFiguras.py

--- CalculadoraFiguras.py
+++ CalculadoraFiguras.py
@@ -11,15 +11,14 @@ with tabs[0]:
    figura = ["Círculo", "Triángulo", "Rectángulo", "Cuadrado"]
    figura_seleccionada = st.selectbox("Selecciona la figura", figura)
    st.write("Figura: ", figura_seleccionada)
-   color = st.color_picker("Selecciona un color: ", "#1f77b4")
+   color = st.color_picker("Selecciona un color: ", "#10B4E0")
 
 if figura == "Círculo":
-   print("Fórmula: ", st.latex("A=π * r²"))
-   r = st.number_input("Radio")
-   area = np.pi * r**2
-   print("Área: ", area)
-   perimetro = 2 * np.pi * r
-   print("Perímetro: ", perimtro)
+   r = st.number_input("Radio", min_value=0.0, value=1)
+   area =("Área: ", "np.pi * r**2")
+   formula =("Fórmula: ", st.latex("A=π * r²"))
+   perimetro =("Perimetro: ", "2 * np.pi * r")
+  
 
 
 
